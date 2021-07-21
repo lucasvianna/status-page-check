9ce4eca1079c49c8744c7f20b391cc5ae9251721
arthurlm44 <arthurlm44@gmail.com>
2021-07-21 16:15:51 -0700

    call lucas code from server

--- statuspage/web-server/server.py
+++ statuspage/web-server/server.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    foo = subprocess.call("python3 status-page-check.py", shell=True)
+    return(foo)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
