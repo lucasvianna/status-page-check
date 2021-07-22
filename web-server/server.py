79851ddf6dc27601dad5bc8405fe7984db3f72eb
arthurlm44 <arthurlm44@gmail.com>
2021-07-22 10:28:40 -0700

    integrate lucas code'

--- web-server/server.py
+++ web-server/server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 import subprocess
 import serverconf
+import status_page_check as sp
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    foo = subprocess.getoutput("python3 status_page_check.py --failed")
-    return(foo)
+    results = sp.web_version()
+    return(results)
 
 if __name__ == "__main__":
     app.run(host=serverconf.serverSettings, debug=True, port=5000)
